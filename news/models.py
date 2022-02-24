c8c2c284c53c931dd09ac588665ab6979ef809c0
dmitrushagood <93004624+dmitrushagood@users.noreply.github.com>
2022-02-24 16:15:04 +0300

    Initial commit

---
+++ news/models.py
@@ -0,0 +1,91 @@
+
+from django.contrib.auth.models import User
+from django.db import models
+from django.db.models import Sum
+
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_rate = models.IntegerField(default=0)
+
+    def update_rating(self):
+        # суммарный рейтинг всех комментариев к статьям
+        sum_rating = self.post_set.aggregate(post_rating=Sum('post_rate'))
+        result_sum_rating = 0
+        try:
+            result_sum_rating += sum_rating.get('post_rating')
+        except TypeError:
+            result_sum_rating = 0
+
+        # суммарный рейтинг всех комментариев самого автора
+        sum_comment_rating = self.user.comment_set.aggregate(comment_rating=Sum('comment_rate'))
+        result_sum_comment_rating = 0
+        result_sum_comment_rating += sum_comment_rating.get('comment_rating')
+
+        # суммарный рейтинг каждой статьи автора умноженный на 3
+        self.user_rate = result_sum_rating * 3 + result_sum_comment_rating
+        # сохраняем результаты в базу данных
+        self.save()
+
+
+class Category(models.Model):
+    name_category = models.CharField(max_length=52, unique=True)
+
+
+class Post(models.Model):
+    post = models.ForeignKey(Author, on_delete=models.CASCADE)  # связь «один ко многим» с моделью Author;
+
+    article = 'AR'
+    news = 'NE'
+
+    posts = [
+        (article, 'Статья'),
+        (news, 'Новость')
+    ]
+    choice = models.CharField(max_length=2,  # поле с выбором — «статья» или «новость»;
+                              choices=posts,
+                              default=news)
+
+    created = models.DateTimeField(auto_now_add=True)  # автоматически добавляемая дата и время создания;
+
+    Post = models.ManyToManyField(Category, through='PostCategory')  # связь «многие ко многим» с моделью Category
+    # (с дополнительной моделью PostCategory);
+
+    title = models.CharField(max_length=150)  # заголовок статьи/новости;
+
+    text = models.TextField()  # текст статьи/новости;
+
+    post_rate = models.IntegerField(default=0)  # рейтинг статьи/новости.
+
+    def like(self):
+        self.post_rate += 1
+        self.save()
+
+    def dislike(self):
+        self.post_rate -= 1
+        self.save()
+
+    def preview(self):
+        size = 124 if len(self.text) > 124 else len(self.text)
+        return self.text[:size] + '...'
+
+
+class PostCategory(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)  # связь «один ко многим» с моделью Post
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    comment_rate = models.IntegerField(default=0)
+
+    def like(self):
+        self.comment_rate += 1
+        self.save()
+
+    def dislike(self):
+        self.comment_rate -= 1
+        self.save()
